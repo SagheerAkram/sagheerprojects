20f4dff18b78eb39eccefdf341c7153eb326c07e
Sagheer <sagheer@example.com>
2026-03-16 16:42:43 +0500

    feat: trigger freedom state after theory exams (April 15)

---
+++ dist/chunks/sagheer-akram-photography-stillness-sunday-silence_eN_mVU_N.mjs
@@ -0,0 +1,14 @@
+const sagheerAkramPhotographyStillnessSundaySilence = new Proxy({"src":"/_astro/sagheer-akram-photography-stillness-sunday-silence.C1nF40SV.jpg","width":1080,"height":1080,"format":"jpg"}, {
+						get(target, name, receiver) {
+							if (name === 'clone') {
+								return structuredClone(target);
+							}
+							if (name === 'fsPath') {
+								return "C:/Users/SM/Desktop/Github Projects/sagheerprojects/src/assets/photography/sagheer-akram-photography-stillness-sunday-silence.jpg";
+							}
+							if (target[name] !== undefined && globalThis.astroAsset) globalThis.astroAsset?.referencedImages.add("C:/Users/SM/Desktop/Github Projects/sagheerprojects/src/assets/photography/sagheer-akram-photography-stillness-sunday-silence.jpg");
+							return target[name];
+						}
+					});
+
+export { sagheerAkramPhotographyStillnessSundaySilence as default };
